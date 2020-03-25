1e0a5fd45332286c163702eb4242ad214c107e2e
sierakowskak <216885@edu.p.lodz.pl>
2020-03-25 12:10:55 +0100

    base project

---
+++ src/PopupDialogAction.java
@@ -0,0 +1,25 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.pom.Navigatable;
+import org.jetbrains.annotations.NotNull;
+
+public class PopupDialogAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent ) {
+        String coronaData = "tralala";
+        Project currentProject = anActionEvent.getProject();
+        Messages.showMessageDialog(currentProject, coronaData, "Ile już jest corony?", Messages.getInformationIcon());
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        e.getPresentation().setEnabledAndVisible(project != null);
+    }
+
+
+}
